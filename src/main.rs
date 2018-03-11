d5a4736c63efe598d8a7e4819c4c16b0d77796ef
Felix Schütt <felix.schuett@maps4print.com>
2018-03-11 04:02:47 +0100

    Adjusted RPC, refined main task view

--- src/main.rs
+++ src/main.rs
@@ -6,43 +6,12 @@ extern crate serde_derive;
 extern crate serde_json;
 extern crate urlencoding;
 extern crate web_view;
+extern crate procinfo;
 
 use urlencoding::encode;
 use web_view::*;
 
-fn webview_cb<'a>(webview: &mut WebView<'a, Vec<Task>>, arg: &str, tasks: &mut Vec<Task>) {
-    use Cmd::*;
-    match serde_json::from_str(arg).unwrap() {
-        init => (),
-        log { text } => println!("{}", text),
-        addTask { name } => tasks.push(Task { name, done: false }),
-        markTask { index, done } => tasks[index].done = done,
-        clearDoneTasks => tasks.retain(|t| !t.done),
-    }
-    render(webview, tasks);
-}
-
-fn main() {
-    let app_html = include_str!("dist/app.html");
-    let url = "data:text/html,".to_string() + &encode(&app_html);
-    let size = (840, 630);
-    let resizable = true;
-    let debug = true;
-    let init_cb = |_webview| {};
-    let userdata = vec![];
-    let (_, launched_successful) = run("Rust Todo App", &url, Some(size), resizable, debug, init_cb, |webview, arg, tasks: &mut Vec<Task>| {
-        webview_cb(webview, arg, tasks);
-    }, userdata);
-
-    if !launched_successful {
-        println!("failed to launch {}", env!("CARGO_PKG_NAME"));
-    }
-}
-
-fn render<'a, T>(webview: &mut WebView<'a, T>, tasks: &[Task]) {
-    // println!("{:#?}", tasks);
-    webview.eval(&format!("rpc.render({})", serde_json::to_string(tasks).unwrap()));
-}
+const APP_TITLE: &str = "Task Manager";
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Task {
@@ -53,9 +22,47 @@ struct Task {
 #[derive(Deserialize)]
 #[serde(tag = "cmd")]
 pub enum Cmd {
-    init,
-    log { text: String },
-    addTask { name: String },
-    markTask { index: usize, done: bool },
-    clearDoneTasks,
+    #[serde(rename = "init")]
+    Init,
+    #[serde(rename = "log")]
+    Log { text: String },
+    #[serde(rename = "update_stuff")]
+    UpdateStuff,
+}
+
+fn webview_cb<'a>(webview: &mut WebView<'a, Vec<Task>>, arg: &str, tasks: &mut Vec<Task>) {
+    
+    match serde_json::from_str::<Cmd>(arg) {
+        Ok(arg) => {
+            match arg {
+                Cmd::Init => (),
+                Cmd::Log { text } => println!("{}", text),
+                Cmd::UpdateStuff => { println!("Updating stuff"); }
+            }
+        },
+        Err(e) => {
+            println!("[Rust] Got garbage RPC from JS: {:?}\r\n\r\n{:?}", e, arg);
+        }
+    }
+
+    webview.eval(&format!("rpc.render({})", serde_json::to_string(tasks).unwrap()));
+}
+
+fn main() {
+    let app_html = include_str!("dist/app.html");
+    let url = "data:text/html,".to_string() + &encode(&app_html);
+    let size = (840, 630);
+    let resizable = true;
+    let debug = true;
+
+    let init_cb = |_webview| {};
+
+    let userdata = vec![];
+    let (_, launched_successful) = run(APP_TITLE, &url, Some(size), resizable, debug, init_cb, |webview, arg, tasks: &mut Vec<Task>| {
+        webview_cb(webview, arg, tasks);
+    }, userdata);
+
+    if !launched_successful {
+        println!("failed to launch {}", env!("CARGO_PKG_NAME"));
+    }
 }
--- src/views/main_view/view.js
+++ src/views/main_view/view.js
@@ -1,24 +1,21 @@
-var rpc = {
+let rpc = {
   invoke : function(arg) { window.external.invoke(JSON.stringify(arg)); },
-  init : function() { rpc.invoke({cmd : 'init' }); },
-  log : function() {
-    var s = '';
-    for (var i = 0; i < arguments.length; i++) {
-      if (i != 0) {
-        s = s + ' ';
-      }
-      s = s + JSON.stringify(arguments[i]);
+  init : function() { rpc.invoke({ cmd : 'init' }); },
+  log : function(text_to_log) {
+    if (typeof text_to_log !== 'string') {
+      text_to_log = JSON.stringify(text_to_log);
     }
-    rpc.invoke({cmd : 'log', text : s});
-  },
-  addTask : function(name) { rpc.invoke({cmd : 'addTask', name : name}); },
-  clearDoneTasks : function() { rpc.invoke({cmd : 'clearDoneTasks'}); },
-  markTask : function(index, done) {
-    rpc.invoke({cmd : 'markTask', index : index, done : done});
+    rpc.invoke({cmd: 'log', text: text_to_log});
   },
   render : function(items) {
+    // This function is called from the Rust side, we need to update the given elements here ... 
     // return element = picodom.patch(oldNode, (oldNode = UI(items)), element);
   },
+
 };
+
+document.getElementById("global_menu_file").addEventListener("click", function() {
+    rpc.invoke({ cmd : 'update_stuff' });
+}, false);
 
 window.onload = function() { rpc.init(); };
